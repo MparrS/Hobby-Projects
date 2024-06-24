7794e620bd935f4e9e7d3964c0d36acc95a93e9f
MparrS <mserranoparra35@gmail.com>
2024-06-23 21:30:47 -0500

    Edits

--- Sliders/slider-2/app.js
+++ Sliders/slider-2/app.js
@@ -1,9 +1,8 @@
-//step 1: get DOM
 let nextDom = document.getElementById('next');
 let prevDom = document.getElementById('prev');
 
 let carouselDom = document.querySelector('.carousel');
-let SliderDom = carouselDom.querySelector('.carousel .list');
+ let SliderDom = carouselDom.querySelector('.carousel .list');
 let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
 let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
 let timeDom = document.querySelector('.carousel .time');
